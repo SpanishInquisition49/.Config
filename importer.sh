d2d9a372d1aa9006cd9543b75563c099ef79da82
Gabriele <71408527+SpanishInquisition49@users.noreply.github.com>
2022-06-20 12:56:31 +0200

    Added Brewfile

--- importer.sh
+++ importer.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ln -s "$(pwd)/.gitconfig" ~/.gitconfig
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap Homebrew/bundle
+brew bundle --file Brewfile
 echo "Done!"
