375c07fda3f76a6049c88a61517012a076d45a75
Gabriele <71408527+SpanishInquisition49@users.noreply.github.com>
2022-06-29 14:54:28 +0200

    Converted the script for oh-my-zsh

--- importer.sh
+++ importer.sh
@@ -61,12 +61,27 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     mv font/fonts/ttf/* /Library/Fonts
 fi
 fc-cache -f -v
+
+# Installing oh-my-zsh
+echo "Installing Oh My ZSH"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm ~/.zshrc
+rm ~/.zshrc.pre-oh-my-zsh
+mv ~/.oh-my-zsh "(pwd)/dotfiles/"
+
+# Installing Dracula Theme for ZSH
+git clone https://github.com/dracula/zsh.git "$(pwd)/dotfiles/.zsh_theme/dracula"
+ln -s $(pwd)/dotfiles/.zsh_theme/dracula.zsh-theme $(pwd)/dotfiles/.oh-my-zsh/themes/dracula.zsh-theme
+echo "Installing ZSH plugins"
+# Installing ZSH Plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $(pwd)dotfiles/.oh-my-zsh/plugins/zsh-syntax-highlighting/
+git clone https://github.com/zsh-users/zsh-autosuggestions $(pwd)dotfiles/.oh-my-zsh/plugins/zsh-autosuggestions/
 echo "Creating Symbolik links for .files"
 # Create Symbolic Link
 ln -s "$(pwd)/dotfiles/.gitconfig" ~/.gitconfig
 ln -s "$(pwd)/dotfiles/.bash_profile" ~/.bash_profile
+ln -s "$(pwd)/dotfiles/.oh-my-zsh" ~/.oh-my-zsh
 ln -s "$(pwd)/dotfiles/.zshrc" ~/.zshrc
-ln -s "$(pwd)/dotfiles/.zsh_plugins" ~/.zsh_plugins
 # Importing VS Code settings.json
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     ln -s "$(pwd)/dotfiles/settings.json" ~/.config/Code/User/settings.json
@@ -74,10 +89,4 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     ln -s "$(pwd)/dotfiles/settings.json" ~/Library/"Application Support"/Code/User/settings.json
 fi
-echo "Installing ZSH plugins"
-# Installing ZSH Plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git dotfiles/.zsh_plugins/zsh-syntax-highlighting/
-echo "source $HOME/.zsh_plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions dotfiles/.zsh_plugins/zsh-autosuggestions/
-echo "source $HOME/.zsh_plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 echo "Done!"
